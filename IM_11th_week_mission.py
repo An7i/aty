1466a9503569c5a9160c5c7ace7e10960a843db9
An7i <atyantivirus@gmail.com>
2022-01-29 18:35:01 +0900

    my first commit

---
+++ IM_11th_week_mission.py
@@ -0,0 +1,35 @@
+# Ai반 2기 python 중급반 - 11주차 정규수업(16:30~18:30) mission
+'''
+[수업을 시작하기 전에!]
+1. 웨일ON으로 원격 회의실 접속하기
+2. NAS 서버 접속(접속이 안될 경우 말해주기~)
+3. 탐색기를 활용하여 내가 현재 작업하고 있는 파일 경로 열어두기
+4. Pycharm 수업 코드 다운로드 받아 열어두기~
+5. 수업시간 집중~~~!! 딴 짓! 멈춰~~~~~!
+'''
+
+# Problem 1: 더하기 사이클
+
+# dummy = copy.copy(N)
+# count = 0
+# if N < 10:
+#     N *= 10
+# while True:
+#     if dummy < 10:
+#         dummy *= 10
+#
+#     dummy = () + ()
+#
+#     if N == dummy:
+#         break
+
+# Problem 2: 평균은 넘겠지
+N = int(input())
+
+
+
+# Problem 3: 셀프 넘버
+
+
+# 추가 문제: ACM 호텔
+
